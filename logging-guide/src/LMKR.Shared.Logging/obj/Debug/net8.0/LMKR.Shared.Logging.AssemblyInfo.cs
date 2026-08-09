9bba90c35016003bc7edb20d125401af8956f36b
Jalal Hussain <HJHussain@lmkr.com>
2026-08-09 15:05:09 +0500

    updated

--- logging-guide/src/LMKR.Shared.Logging/obj/Debug/net8.0/LMKR.Shared.Logging.AssemblyInfo.cs
+++ logging-guide/src/LMKR.Shared.Logging/obj/Debug/net8.0/LMKR.Shared.Logging.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Shared Serilog + request/response logging middleware for all LMKR microservices.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8495facf7c899e49efc1fe8b1aae4dc66b29453")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfdb09812c4a5f839201a12ea9187f98a1d55013")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMKR.Shared.Logging")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMKR.Shared.Logging")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
